2962f4d75df2ca886bd415a68d35d81fc0cb762a
tihoson <alvi99@inbox.ru>
2022-11-29 23:41:36 +0200

    add lab3 logging

--- lab3/main.go
+++ lab3/main.go
@@ -17,25 +17,33 @@ func main() {
 }
 
 func worker(url, path string) {
+	log.Printf("---------------\nstart new job\nurl -> %s\npath\n%s\n---------------\n", url, path)
+
 	out, err := os.Create(path)
 	if err != nil {
-		log.Println(err)
+		log.Printf("create file error: %v", err)
+		return
 	}
 	defer out.Close()
+	log.Printf("create file %s\n", path)
 
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Println(err)
+		log.Printf("http get error: %v", err)
+		return
 	}
 	defer resp.Body.Close()
+	log.Printf("response from %s for file %s received\n", url, path)
 
 	if resp.StatusCode != http.StatusOK {
-		log.Println(resp.StatusCode)
+		log.Printf("http response code is %d not %d", resp.StatusCode, http.StatusOK)
+		return
 	}
 
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		log.Println(err)
+		log.Printf("body copy error: %v", err)
+		return
 	}
-	fmt.Println(path, " done")
+	fmt.Printf("---------------\njob is done\nurl -> %s\npath\n%s\n---------------\n", url, path)
 }
